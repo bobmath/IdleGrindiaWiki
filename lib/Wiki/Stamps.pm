b8f02b3b3e2065690f349acc6955ee136d0c9a2e
Bob Mathews <bobmath11@icloud.com>
2020-09-06 11:58:12 -0700

    stamp icons

--- lib/Wiki/Stamps.pm
+++ lib/Wiki/Stamps.pm
@@ -7,17 +7,17 @@ sub build {
    my ($ctx) = @_;
    open my $OUT, '>:utf8', 'wiki/Stamps' or die;
    print $OUT qq[{| class="wikitable"\n],
-      "|-\n! Name || Cost || Effect\n";
+      "|-\n! Icon || Name || Cost || Effect\n";
    my $data = $ctx->get_objects('DailyRewardData');
 
    for my $i (0 .. $#{$data->{title}}) {
       if ($i % 5 == 0) {
          my $tier = $i / 5;
-         my $txt = "Tier " . ($tier + 1);
+         my $txt = "'''Tier " . ($tier + 1) . "'''";
          if (my $req = $data->{required}[$tier]) {
             $txt .= " (requires $req stamps)";
          }
-         print $OUT "|-\n| colspan=3 | $txt\n";
+         print $OUT "|-\n| colspan=4 | $txt\n";
       }
       my @cost;
       if (my $hunt = $data->{hunter}[$i]) {
@@ -26,7 +26,8 @@ sub build {
       if (my $expl = $data->{explorer}[$i]) {
          push @cost, "{{Explorer|$expl}}";
       }
-      print $OUT "|-\n| ", Grindia::trim($data->{title}[$i]), "\n",
+      my $name = Grindia::trim($data->{title}[$i]);
+      print $OUT "|-\n| [[File:$name.png|24px|center]]\n| $name\n",
          "| @cost\n| ", Grindia::trim($data->{text}[$i]), "\n";
    }
 
